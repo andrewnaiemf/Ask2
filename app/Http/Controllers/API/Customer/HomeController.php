b3ede1bf293ca73c28d04b15adfcda256d65daa8
andrewnaiemf <andrewnaiemf@gmail.com>
2023-07-15 00:05:49 +0300

    add validation for search for api

--- app/Http/Controllers/API/Customer/HomeController.php
+++ app/Http/Controllers/API/Customer/HomeController.php
@@ -7,6 +7,7 @@ use App\Models\Department;
 use App\Models\Provider;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class HomeController extends Controller
 {
@@ -45,6 +46,15 @@ class HomeController extends Controller
 
     public function search($searchTerm, Request $request){
 
+        $validator = Validator::make($request->all(), [
+
+            'search_for' => 'required|In:department,provider',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->returnValidationError(401,$validator->errors()->all());
+        }
+
         $perPage = $request->header('per_page', 10);
 
         if ($request->search_for == 'provider') {
@@ -52,7 +62,7 @@ class HomeController extends Controller
             $data = User::where('account_type', 'provider')
             ->where('name', 'LIKE', '%' . $searchTerm . '%')
             ->simplePaginate($perPage);
-        }else{
+        }else if($request->search_for == 'department'){
 
             $data = Department::where('name_'.app()->getLocale(), 'LIKE', '%' . $searchTerm . '%')
             ->simplePaginate($perPage);
