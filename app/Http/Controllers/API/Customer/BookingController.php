4b2152b58a99c52fb87654a25c60ac8cd18a51f2
andrewnaiemf <andrewnaiemf@gmail.com>
2023-08-13 01:27:42 +0300

    room booking

--- app/Http/Controllers/API/Customer/BookingController.php
+++ app/Http/Controllers/API/Customer/BookingController.php
@@ -7,7 +7,10 @@ use App\Models\Booking;
 use App\Models\BookingDetails;
 use App\Models\Clinic;
 use App\Models\ClinicBooking;
+use App\Models\HotelBookingDetails;
+use App\Models\HotelBookingRoom;
 use App\Models\Provider;
+use App\Models\Room;
 use App\Models\User;
 use App\Notifications\PushNotification;
 use Illuminate\Http\Request;
@@ -71,7 +74,10 @@ class BookingController extends Controller
         }
 
         if ($request['booking_type'] == 'Hotel') {
-            $this->hotelBooking($request, $booking);
+            $response = $this->hotelBooking($request, $booking);
+            if ($response) {
+                return $this->returnError($response);
+            }
         }
 
         PushNotification::create($booking->user_id ,$provider->user->id ,$booking ,'booking');
@@ -101,7 +107,44 @@ class BookingController extends Controller
     }
 
     public function hotelBooking($request, $booking){
-       dd('a');
+
+        $room_id = $request->input('room_id');
+        try {
+            \DB::beginTransaction();
+
+            $room = Room::lockForUpdate()->find($room_id); // Lock the room for update
+            if ($room->busy_numbers >= $room->numbers) {
+                throw new \Exception('The room is fully booked.');
+            }
+            $hotelBookingDetails = new HotelBookingDetails();
+            $hotelBookingDetails->year = $request->input('year');
+            $hotelBookingDetails->arrival_month = $request->input('arrival_month');
+            $hotelBookingDetails->arrival_day = $request->input('arrival_day');
+            $hotelBookingDetails->arrival_time = $request->input('arrival_time');
+            $hotelBookingDetails->departure_month = $request->input('departure_month');
+            $hotelBookingDetails->departure_day = $request->input('departure_day');
+            $hotelBookingDetails->departure_time = $request->input('departure_time');
+            $hotelBookingDetails->total_cost = $request->input('total_cost');
+            $hotelBookingDetails->adults = $request->input('adults');
+            $hotelBookingDetails->kids = $request->input('kids');
+
+            $booking->hotelBookingDetail()->save($hotelBookingDetails);
+
+
+            $booking_room = new HotelBookingRoom();
+            $booking_room->room_id = $room_id;
+            $booking_room->booking_id = $booking->id;
+
+
+            $room = Room::find($room_id);
+            $room->update(['busy_numbers' => $room->busy_numbers + 1]);
+            $booking->hotelBookingDetail->roomBookingDetail()->save($booking_room);
+            \DB::commit();
+        } catch (\Exception $e) {
+            \DB::rollback();
+            $booking->forceDelete();
+            return $e->getMessage();
+        }
     }
 
     public function validateBookingData ( $request ) {
@@ -147,14 +190,58 @@ class BookingController extends Controller
          // Validation rules for hotel booking
         $hotelRules = [
             'year' => 'required_with:arrival_month,departure_month|integer|date_format:Y|in:' . date('Y'),
-            'arrival_month' => 'required_with:year|digits:2|between:01,12',
-            'arrival_day' => 'required_with:year|integer|between:01,31',
+            'arrival_month' => [
+                'required',
+                'digits:2',
+                'between:01,12',
+                function ($attribute, $value, $fail) {
+                    $currentMonth = date('m');
+                    if ($value < $currentMonth || $value > $currentMonth + 1) {
+                        $fail('The '.$attribute.' must be the current month or the next month.');
+                    }
+                }
+            ],
+            'arrival_day' => [
+                'required',
+                'digits:2',
+                'between:01,31',
+                function ($attribute, $value, $fail) {
+                    $currentDay = date('d');
+                    if ($value < $currentDay) {
+                        $fail('The '.$attribute.' must be today or a future date.');
+                    }
+                }
+            ],
             'arrival_time' => 'required_with:year|date_format:H:i',
-            'departure_month' => 'required_with:year|digits:2|between:01,12',
-            'departure_day' => 'required_with:year|integer|between:01,31',
+            'departure_month' => [
+                'required',
+                'digits:2',
+                'between:01,12',
+                'after_or_equal:arrival_month',
+                function ($attribute, $value, $fail) {
+                    $currentMonth = date('m');
+                    if ($value < $currentMonth || $value > $currentMonth + 1) {
+                        $fail('The '.$attribute.' must be the current month or the next month.');
+                    }
+                }
+            ],
+            'departure_day' => [
+                'required',
+                'digits:2',
+                'between:01,31',
+                'after_or_equal:arrival_day',
+                function ($attribute, $value, $fail) {
+                    $currentDay = date('d');
+                    if ($value < $currentDay) {
+                        $fail('The '.$attribute.' must be today or a future date.');
+                    }
+                }
+            ],
             'departure_time' => 'required_with:year|date_format:H:i',
             'adults' => 'required_with:year|integer|min:1',
             'kids' => 'required_with:year|integer|min:0',
+            'total_cost' => 'required_with:year|integer',
+            'room_id' => 'required_with:year|integer|exists:rooms,id',
             'booking_type' => 'required_with:year|in:Hotel',
         ];
 
