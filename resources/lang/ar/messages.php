f086c8351fcd5be41d6d23848d0d5118819cc388
andrewnaiemf <andrewnaiemf@gmail.com>
2023-08-20 23:09:49 +0300

    fix translation

--- resources/lang/ar/messages.php
+++ resources/lang/ar/messages.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'rating_message' => ':sender_name قدم لك :stars نجمة :stars',
+    'rating_message' => ':sender_name قدم لك :stars نجمة ',
     'New_booking' => ':sender_name  قام بحجز جديد ',
     'provider_accepted_booking' => ' قبل حجزك',
     'provider_rejected_booking' => ' رفض حجزك',
