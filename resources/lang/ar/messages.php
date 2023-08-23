5d7e9b986dd4ce0edab006c7baf9f34407ba0ff9
andrewnaiemf <andrewnaiemf@gmail.com>
2023-08-23 21:19:05 +0300

    translate accept and reject booking

--- resources/lang/ar/messages.php
+++ resources/lang/ar/messages.php
@@ -3,6 +3,6 @@
 return [
     'rating_message' => ':sender_name قدم لك :stars نجمة ',
     'New_booking' => ':sender_name  قام بحجز جديد ',
-    'provider_accepted_booking' => ' قبل حجزك',
-    'provider_rejected_booking' => ' رفض حجزك',
+    'provider_Completed_booking' => ' قبل حجزك',
+    'provider_Rejected_booking' => ' رفض حجزك',
 ];
--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -3,6 +3,6 @@
 return [
     'rating_message' => ':sender_name gave you :stars stars',
     'New_booking' => ':sender_name make new booking',
-    'provider_Completed_booking' => 'accepted your booking',
-    'provider_Rejected_booking' => 'rejected your booking',
+    'provider_Completed_booking' => 'complete your booking',
+    'provider_Rejected_booking' => 'rejecte your booking',
 ];
